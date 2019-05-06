b40a02d9085daa252cef1d1167c7ca19535b5a2c
Jack Hilscher <Jack@Jacks-MacBook-Air.local>
2019-05-06 11:33:24 -0400

    prevents actions while doing something, and displays message

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const gameContent = document.querySelector(".memory-game")
 const messages = document.querySelector(".messages")
 
 let choice = ""
+let doingSomething = ""
 
 
 
@@ -14,6 +15,8 @@ symbols.forEach(function(sym){
 
 document.addEventListener("DOMContentLoaded", function (e){
   const cardText = document.querySelectorAll(".card-text")
+  doingSomething = "yes"
+  messages.innerHTML = "Pay Attention!!"
   let i = 0
   const double = (2 * cardText.length) - 1
   cardText.forEach(function(card){
@@ -21,66 +24,68 @@ document.addEventListener("DOMContentLoaded", function (e){
       if (!card.style.opacity) {
           card.style.opacity = 1;
           console.log(1)
-      }
-      else if (i > double) {
+      } else if (i > double) {
         card.style.opacity = 0
 // TODO: make the text white instead of transluscent
         clearInterval(fadeEffect)
         console.log(3)
-
-      }
-      else if (card.style.opacity > 0) {
+        doingSomething = ""
+        messages.innerHTML = ""
+      } else if (card.style.opacity > 0) {
           card.style.opacity -= 0.01;
           console.log(2)
-
-      }
-      else if (card.style.opacity == 0) {
+      } else if (card.style.opacity == 0) {
         card.style.opacity = 1;
         i++
         console.log(4)
+      } else {
+        clearInterval(fadeEffect);
       }
-      else {
-          clearInterval(fadeEffect);
-      }
-  }, 10);
+    }, 10);
   //when user clicks the card we remove it
-
-})//end of opacity Fn
-
+  })//end of opacity Fn
 
 })//end of DOMContentLoaded
 
-document.addEventListener("click", function(e){
 
-  if ((e.target.nodeName === "SPAN") && (e.target.firstElementChild.style.opacity !== "1")){
-    if (choice === ""){
+  //prevents clicking while
 
-      choice = e.target.dataset.id
-      e.target.firstElementChild.style.opacity = 1
-    }else if (e.target.dataset.id === choice){
-      e.target.firstElementChild.style.opacity = 1
-      const chosen = document.getElementsByClassName(choice)
-      Array.from(chosen).forEach(function(choice){
-        choice.style.color = "red"
-      })
-      choice = ""
-    } else if (e.target.dataset.id !== choice){
-
-      e.target.firstElementChild.style.opacity = 1
-      const wrong = document.getElementsByClassName(choice)
-      messages.innerHTML = "These do not match"
-      // alert("These don't match!")
+  document.addEventListener("click", function(e){
+    if (doingSomething === ""){
 
 
-      choice = ""
-      setTimeout(function(){
-        e.target.firstElementChild.style.opacity = 0
-        Array.from(wrong).forEach(function(choice){
-          choice.firstElementChild.style.opacity  = 0
-        })
-        messages.innerHTML = ""
+      if ((e.target.nodeName === "SPAN") && (e.target.firstElementChild.style.opacity !== "1")){
+        if (choice === ""){
 
-      },1000);
+          choice = e.target.dataset.id
+          e.target.firstElementChild.style.opacity = 1
+        } else if (e.target.dataset.id === choice){
+          e.target.firstElementChild.style.opacity = 1
+          const chosen = document.getElementsByClassName(choice)
+          Array.from(chosen).forEach(function(choice){
+            choice.style.color = "red"
+          })
+          choice = ""
+        } else if (e.target.dataset.id !== choice){
+          e.target.firstElementChild.style.opacity = 1
+          const wrong = document.getElementsByClassName(choice)
+          messages.innerHTML = "These do not match"
+
+
+          choice = ""
+          doingSomething = "yes"
+          setTimeout(function(){
+            e.stopPropagation()
+            e.target.firstElementChild.style.opacity = 0
+            Array.from(wrong).forEach(function(choice){
+              choice.firstElementChild.style.opacity  = 0
+            })
+            messages.innerHTML = ""
+
+            doingSomething = ""
+          },1000);
+
+        }
+      }
     }
-  }
-})
+  })
